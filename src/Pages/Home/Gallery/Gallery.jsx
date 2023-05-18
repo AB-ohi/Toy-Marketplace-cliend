e169ae661f93445a4154e0ccdf86b8c2eadbd0bc
AB-ohi <askohi63907@gmail.com>
2023-05-18 20:08:01 +0600

    damo array for gallary

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,48 @@
+
+const Gallery = () => {
+  const  toys = [
+      {
+          _id: 1,
+          name: "LEGO Classic Creative Bricks",
+          ImageUrl: ,
+          
+      },
+      {
+          _id: 2,
+          name: "Barbie Dreamhouse",
+          ImageUrl: ,
+          
+      },
+      {
+          _id: 3,
+          name: "Hot Wheels 20-Car Gift Pack",
+          ImageUrl: ,
+          
+      },
+      {
+          _id: 4,
+          name: "Nerf N-Strike Elite Disruptor Blaster",
+          ImageUrl: ,
+          
+      },
+      {
+          _id: 5,
+          name: "Baby Yoda Plush Toy",
+          ImageUrl: ,
+          
+      },
+      {
+          _id: 6,
+          name: "Fisher-Price Rock-a-Stack",
+          ImageUrl: ,
+          
+      }
+  ]
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Gallery;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,11 @@
-import About from "../About/About";
 import Banner from "../Banner/Baner";
+import Gallery from "../Gallery/Gallery";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <About></About>
+            <Gallery></Gallery>
         </div>
     );
 };
