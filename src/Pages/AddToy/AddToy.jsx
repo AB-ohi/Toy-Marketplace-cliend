9de42e070e7b066f259a08d7ae131e3f1c771a7f
AB-ohi <askohi63907@gmail.com>
2023-05-19 23:32:04 +0600

    data pasing

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -1,6 +1,23 @@
 const AddToy = () => {
 
-    
+    const handelAddToy = event =>{
+        event.preventDefault()
+
+        const form = event.target;
+        const name = form.name.value;
+        const sellerName =form.sellerName.value;
+        const email = form.email.value;
+        const SubCategory = form.SubCategory.value;
+        const price = form.price.value;
+        const quantity = form.quantity.value;
+        const picture = form.picture.value;
+        const newToy = {name, sellerName, email, SubCategory, price, quantity, picture }
+        console.log(newToy)
+
+        fetch('http://localhost:5000/addToy')
+    }
+
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -8,13 +25,14 @@ const AddToy = () => {
           <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
             <div className="card-body">
               <h1 className="text-center font-semibold text-5xl">Add Toy</h1>
-              <form>
+              <form onSubmit={handelAddToy}>
                 <div className="md:flex gap-6">
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">Name</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Toy Name"
                       name="name"
@@ -26,6 +44,7 @@ const AddToy = () => {
                       <span className="label-text">seller name</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Seller Name"
                       name="sellerName"
@@ -39,6 +58,7 @@ const AddToy = () => {
                       <span className="label-text">Seller Email</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Email"
                       name="email"
@@ -50,6 +70,7 @@ const AddToy = () => {
                       <span className="label-text">Sub-category</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Sub-category"
                       name="SubCategory"
@@ -63,6 +84,7 @@ const AddToy = () => {
                       <span className="label-text">Price</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Price"
                       name="price"
@@ -74,8 +96,10 @@ const AddToy = () => {
                       <span className="label-text">Available quantity</span>
                     </label>
                     <input
+                        required
                       type="text"
                       placeholder="Quantity"
+                      name="quantity"
                       className="input input-bordered"
                     />
                   </div>
@@ -85,6 +109,7 @@ const AddToy = () => {
                     <span className="label-text">Picture URL</span>
                   </label>
                   <input
+                    required
                     type="text"
                     placeholder="URL"
                     name="picture"
@@ -92,7 +117,7 @@ const AddToy = () => {
                   />
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
+                  <input className="btn btn-primary" type="submit" value="submit" />
                 </div>
               </form>
             </div>
