720216ef7c5cce45cfab8d6f32ac5f3e0287b4ee
AB-ohi <askohi63907@gmail.com>
2023-05-19 21:09:20 +0600

    add toy section

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -1,9 +1,40 @@
 const AddToy = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+            <div className="card-body">
+              <form>
+               <div className="flex gap-6">
+               <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="password"
+                    className="input input-bordered"
+                  />
+                </div>
+               </div>
+              </form>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddToy;
